45c12c21b8affe8370d5761a4f620dbcb09790c9
Matt Porritt <mattp@catalyst-au.net>
2021-08-09 04:26:09 +0000

    add lang strings for french

---
+++ lang/fr/format_twocol.php
@@ -0,0 +1,126 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     format_twocol
+ * @category    string
+ * @copyright   2019 Matt Porritt <mattp@catalyst-au.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Deux colonnes';
+$string['privacy:metadata'] = 'Le plugin au format à deux colonnes ne stocke aucune donnée personnelle.';
+
+$string['areachart'] = 'Graphique en aires';
+$string['bell'] = 'Cloche';
+$string['calculator'] = 'Calculatrice';
+$string['calendar'] = 'Calendrier';
+$string['clock'] = 'Horloge';
+$string['completionnag'] = 'Notification des critères d\'achèvement du cours';
+$string['completionnag_desc'] = 'Lorsqu\'il est activé, tous les cours affichent une notification "harcèlement" aux enseignants si les critères d\'achèvement du cours ne sont pas définis pour le cours.';
+$string['completionstats:view'] = 'Afficher les statistiques récapitulatives sur l\'achèvement';
+$string['completionstatus'] = 'Activer les statistiques récapitulatives d\'achèvement';
+$string['completionstatus_label'] = 'Activer';
+$string['completionstatus_help'] = 'Lorsque cette option est activée, les enseignants verront un résumé de l\'état d\'avancement des élèves en haut de la colonne de droite';
+$string['completiontracking'] = 'Activer la reprise d\'activité';
+$string['completiontracking_label'] = 'Activer';
+$string['completiontracking_help'] = 'Lorsqu\'il est activé, un étudiant revenant au cours sera redirigé vers la dernière activité qu\'il regardait';
+$string['detailsheading'] = 'Résumé';
+$string['detailsheading_help'] = 'Choisissez le texte du titre qui s\'affiche en haut de la colonne de droite. C\'est ce qu\'on appelle l\'en-tête Résumé';
+$string['detailsheading_label'] = 'En-tête récapitulatif';
+$string['envelope'] = 'Enveloppe';
+$string['globe'] = 'Globe';
+$string['home'] = 'Accueil du cours';
+$string['home_tip'] = 'Retour à l\'accueil du cours';
+$string['hidefromothers'] = 'Masquer le sujet';
+$string['info'] = 'Information';
+$string['lightning'] = 'Éclair';
+$string['linechart'] = 'Graphique en ligne';
+$string['money'] = 'Argent';
+$string['next'] = 'Prochain:';
+$string['newspaper'] = 'Journal';
+$string['nocompletion'] = 'Aucun <a href="{$a}">critère d\'achèvement</a> n\'est défini pour ce cours. Il est fortement recommandé de définir des <a href="{$a}">critères d\'achèvement</a>.';
+$string['openbook'] = 'Livre ouvert';
+$string['person'] = 'Personne';
+$string['people'] = 'Gens';
+$string['previous'] = 'Précédent:';
+$string['progresstitle:course'] = 'Progression du cours';
+$string['progresstitle:section'] = 'Avancement de la section';
+$string['question'] = 'Point d\'interrogation';
+$string['resourcesheading'] = 'Ressources';
+$string['resourcesheading_help'] = 'Choisissez le texte de l\'en-tête Ressources. C\'est l\'en-tête qui apparaît en bas de la colonne de droite. C\'est le titre de toutes les ressources et activités Moodle dans le premier domaine du cours.';
+$string['resourcesheading_label'] = 'Rubrique ressources';
+$string['reversedisplay'] = 'Affichage de la colonne inversée';
+$string['reversedisplay_label'] = 'Sens inverse';
+$string['reversedisplay_help'] = 'Lorsqu\'il est activé, l\'ordre des colonnes sera inversé, avec l\'image et les sujets du cours sur la droite, et les informations récapitulatives sur la gauche';
+$string['sectionheading1'] = 'Rubrique de l\'article 1 du résumé';
+$string['sectionheading1_help'] = 'Choisissez le texte du premier titre de l\'élément de résumé. Les éléments récapitulatifs apparaissent dans la colonne de droite sous le titre récapitulatif. Ils sont utilisés pour fournir des informations supplémentaires sur les cours aux étudiants.';
+$string['sectionheading1_label'] = 'Rubrique 1 de l\'élément récapitulatif';
+$string['sectionicon1'] = 'Icône de l\'élément de résumé 1';
+$string['sectionicon1_help'] = 'Choisir l\'icône du premier titre de l\'élément de synthèse';
+$string['sectionicon1_label'] = 'Icône de l\'élément de résumé 1';
+$string['sectiontext1'] = 'Texte de l\'élément 1 récapitulatif';
+$string['sectiontext1_help'] = 'Choisir le texte du premier élément récapitulatif';
+$string['sectiontext1_label'] = 'Texte de l\'élément 1 récapitulatif';
+$string['sectionheading2'] = 'Rubrique de l\'article 2 du résumé';
+$string['sectionheading2_help'] = 'Chose the text for the second summary item heading. Summary items appear in the right hand column under the summary heading. They are used to provided extra course information to students.';
+$string['sectionheading2_label'] = 'Rubrique 2 de l\'élément récapitulatif';
+$string['sectionicon2'] = 'Icône de l\'élément de résumé 2';
+$string['sectionicon2_help'] = 'Choisir l\'icône du deuxième titre de l\'élément de synthèse';
+$string['sectionicon2_label'] = 'Icône de l\'élément de résumé 3';
+$string['sectiontext2'] = 'Texte récapitulatif de l\'élément 2';
+$string['sectiontext2_help'] = 'Choisir le texte du deuxième élément de résumé';
+$string['sectiontext2_label'] = 'Texte récapitulatif de l\'élément 2';
+$string['sectionheading3'] = 'Rubrique de l\'article 3 du résumé';
+$string['sectionheading3_help'] = 'Choisissez le texte du troisième titre de l\élément de résumé. Les éléments récapitulatifs apparaissent dans la colonne de droite sous le titre récapitulatif. Ils sont utilisés pour fournir des informations supplémentaires sur les cours aux étudiants.';
+$string['sectionheading3_label'] = 'Rubrique 3 de l\'élément récapitulatif';
+$string['sectionicon3'] = 'Icône de l\'élément de résumé 3';
+$string['sectionicon3_help'] = 'Choisissez l\'icône du troisième titre de l\'élément de synthèse';
+$string['sectionicon3_label'] = 'Icône de l\'élément de résumé 3';
+$string['sectiontext3'] = 'Texte récapitulatif de l\'élément 3';
+$string['sectiontext3_help'] = 'Choisir le texte du troisième élément de résumé';
+$string['sectiontext3_label'] = 'Texte récapitulatif de l\'élément 3';
+$string['sectionheading4'] = 'Rubrique de l\'article 4 du résumé';
+$string['sectionheading4_help'] = 'Choisissez le texte du quatrième titre de l\'élément de résumé. Les éléments récapitulatifs apparaissent dans la colonne de droite sous le titre récapitulatif. Ils sont utilisés pour fournir des informations supplémentaires sur les cours aux étudiants.';
+$string['sectionheading4_label'] = 'Rubrique 4 de l\'élément récapitulatif';
+$string['sectionicon4'] = 'Icône de l\'élément de résumé 4';
+$string['sectionicon4_help'] = 'Choisissez l\'icône du quatrième titre de l\'élément de synthèse';
+$string['sectionicon4_label'] = 'Icône de l\'élément de résumé 4';
+$string['sectiontext4'] = 'Texte récapitulatif de l\'élément 4';
+$string['sectiontext4_help'] = 'Choisir le texte du quatrième élément de résumé';
+$string['sectiontext4_label'] = 'Texte récapitulatif de l\'élément 4';
+$string['sectionheading5'] = 'Rubrique de l\'article 5 du résumé';
+$string['sectionheading5_help'] = 'Choisissez le texte du titre du cinquième élément de résumé. Les éléments récapitulatifs apparaissent dans la colonne de droite sous le titre récapitulatif. Ils sont utilisés pour fournir des informations supplémentaires sur les cours aux étudiants.';
+$string['sectionheading5_label'] = 'Rubrique 5 de l\'élément récapitulatif';
+$string['sectionicon5'] = 'Icône de l\'élément de résumé 5';
+$string['sectionicon5_help'] = 'Choisir l\'icône du cinquième titre de l\'élément de synthèse';
+$string['sectionicon5_label'] = 'Icône de l\'élément de résumé 1';
+$string['sectiontext5'] = 'Texte récapitulatif de l\'élément 5';
+$string['sectiontext5_help'] = 'Choisir le texte du cinquième élément de résumé';
+$string['sectiontext5_label'] = 'Choisir le texte du cinquième élément de résumé';
+$string['section0name'] = 'Aperçu';
+$string['sectionname'] = 'Sujet';
+$string['showfromothers'] = 'Afficher le sujet';
+$string['speedometer'] = 'Compteur de vitesse';
+$string['star'] = 'Star';
+$string['tick'] = 'Cocher';
+$string['userscomplete'] = 'Les étudiants ont terminé';
+$string['usersinprogress'] = 'Etudiants en cours';
+$string['usersnotstarted'] = 'Les élèves n\'ont pas commencé';
