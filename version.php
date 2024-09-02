17b8f39e8c14cedc68b550ac968cc8bf8e698735
rajandangi <rajandangi@catalyst-au.net>
2024-09-02 18:05:40 +0930

    Removed: format_topics dependency in version.php

--- version.php
+++ version.php
@@ -29,7 +29,4 @@ $plugin->release = '2024090200';
 $plugin->version = 2024090200;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->dependencies = array(
-    'format_topics'  => 2019052000,
-);
 $plugin->supported = [404, 404];
