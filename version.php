611439e7db336798002b5b30f1c1665efbdccf26
rajandangi <rajandangi@catalyst-au.net>
2024-09-02 17:46:36 +0930

    Fix: $plugin->requires version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'format_twocol';
 $plugin->release = '2024090200';
 $plugin->version = 2024090200;
-$plugin->requires = 2023120100.01;
+$plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'format_topics'  => 2019052000,
