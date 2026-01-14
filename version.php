19178bf5af878351aeae9d5bcdaff43e4270c2c2
Nathan Nguyen <nathannguyen@catalyst-au.net>
2026-01-14 12:09:08 +1100

    Create new branch for Moodle 5.0

--- version.php
+++ version.php
@@ -29,4 +29,4 @@ $plugin->release = '2025072301';
 $plugin->version = 2025072301;
 $plugin->requires = 2024042200;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->supported = [404, 405];
+$plugin->supported = [500, 501];
